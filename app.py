d49046da0964065d0dbae702a27a9c5371510a95
hammadyasir <hammadyasir343@gmail.com>
2021-03-18 18:40:12 +0500

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
 import os
 import schedule
 import time
 import ctypes
 import creds
-#!/usr/bin/python3
 from selenium import webdriver
 from playsound import playsound
 from selenium.webdriver.support import expected_conditions as EC
