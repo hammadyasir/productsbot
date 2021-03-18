4970ea7682d2fe3f9afc65dd38fc57740ce61a44
hammadyasir <hammadyasir343@gmail.com>
2021-03-18 17:51:12 +0500

    first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from selenium import webdriver 
+
+DRIVER_LOCATION = "/usr/bin/chromedriver" 
+BINARY_LOCATION = "/usr/bin/google-chrome" 
+
+# start selenium
+options = webdriver.ChromeOptions() 
+options.binary_location = BINARY_LOCATION 
+
+driver = webdriver.Chrome(executable_path=DRIVER_LOCATION, options=options) 
+
+driver.get("https://www.imdb.com") 
+print(driver.page_source.encode('utf-8')) 
+
+# close browser and quit driver
+driver.close() 
+driver.quit()
